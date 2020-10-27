965d95840e9547050cae94c6a39dd7ea9b5a934f
robo_nope_rope <xomjak2006@gmail.com>
2020-10-27 16:43:57 +0300

    Part 7

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p bin
+
+nasm bootloader.asm -f bin -o bin/bootloader.bin
+nasm extend.asm -f elf64 -o bin/extend.o
+
+/usr/local/x86_64elfgcc/bin/x86_64-elf-gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c Kernel.cpp -o bin/Kernel.o
+ld -T"linker.ld"
+
+cat bin/bootloader.bin bin/kernel.bin > bin/bootloader.flp
+
+qemu-system-x86_64 -boot order=a -fda bin/bootloader.flp
